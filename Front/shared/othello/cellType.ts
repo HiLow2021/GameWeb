4e848eff243ab5e0e20011e5a1a335c2fa1cbb7d
HiLow <newbie_wisdom@hotmail.com>
2022-08-24 02:50:33 +0900

    clean

--- Front/shared/othello/cellType.ts
+++
@@ -1,9 +0,0 @@
-export type CellType = 'outOfRange' | 'empty' | 'black' | 'white' | 'highLight';
-
-export const CellType = {
-    outOfRange: 'outOfRange',
-    empty: 'empty',
-    black: 'black',
-    white: 'white',
-    highLight: 'highLight'
-} as const;
