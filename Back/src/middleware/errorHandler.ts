280cd92be76414b5691a7db13943a7019d28750d
HiLow <newbie_wisdom@hotmail.com>
2022-09-03 19:48:07 +0900

    clean

--- Back/src/middleware/errorHandler.ts
+++ Back/src/middleware/errorHandler.ts
@@ -1,10 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
 
-const errorHandler = (err: Error, _: Request, res: Response, next: NextFunction): void => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const errorHandler = (err: Error, _: Request, res: Response, __: NextFunction): void => {
     console.error(err);
     res.status(500).send(err.message);
-
-    next(err);
 };
 
 export default errorHandler;
