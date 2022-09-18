a91613a28eef110e57f163c1ad9e931d62087c81
HiLow <newbie_wisdom@hotmail.com>
2022-09-18 17:32:54 +0900

    update game thumbnails

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -6,12 +6,12 @@ import { GameList } from '../shared/game/gameList';
 const Index: NextPage = (): JSX.Element => {
     return (
         <Layout>
-            <div className="flex justify-center gap-4">
+            <div className="flex justify-center gap-8">
                 {GameList.map((game) => (
                     <Link href={game.path}>
                         <a>
                             <div className="flex flex-col">
-                                <img className="h-40 w-40 object-cover" src={game.thumbnail} alt={game.title} />
+                                <img className="h-48 w-48 object-scale-down" src={game.thumbnail} alt={game.title} />
                                 <div className="p-4 text-xl text-center">{game.title}</div>
                             </div>
                         </a>
