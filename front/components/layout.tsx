78130d20bf2efadb21d2ddd18e765f6198068a43
HiLow <newbie_wisdom@hotmail.com>
2022-09-20 20:02:22 +0900

    clean

--- front/components/layout.tsx
+++ front/components/layout.tsx
@@ -4,7 +4,7 @@ export const Layout = ({ children }: { children: React.ReactNode }): JSX.Element
     return (
         <>
             <header className="h-18 top-0 bg-black p-3 text-gray-300">
-                <p className="text-center text-4xl">{Environment.siteTitle}</p>
+                <p className="text-center text-3xl sm:text-4xl">{Environment.siteTitle}</p>
             </header>
             <main className="mx-auto flex min-h-screen w-full max-w-screen-xl flex-grow p-4">
                 <div className="w-full py-4 px-8">{children}</div>
--- front/pages/othello/index.tsx
+++ front/pages/othello/index.tsx
@@ -1,12 +1,11 @@
 import type { NextPage } from 'next';
-import dynamic from 'next/dynamic';
 import GameProvider from '../../components/gameProvider';
 import { Layout } from '../../components/layout';
 
 const Index: NextPage = (): JSX.Element => {
     return (
         <Layout>
-            <GameProvider gameComponentName='othello'/>
+            <GameProvider componentName='othello'/>
         </Layout>
     );
 };
