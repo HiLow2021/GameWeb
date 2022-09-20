5f4f8e8a494e8c84a75b4add79204b5b1ce02fa6
HiLow <newbie_wisdom@hotmail.com>
2022-09-20 20:02:44 +0900

    add gomoku responsive-design

--- front/components/gomoku.tsx
+++ front/components/gomoku.tsx
@@ -1,4 +1,5 @@
-import { Button, FormControl, FormControlLabel, FormLabel, MenuItem, Radio, RadioGroup, Select } from '@mui/material';
+import { Button, FormControl, FormControlLabel, FormLabel, MenuItem, Radio, RadioGroup, Select, Typography } from '@mui/material';
+import { KonvaEventObject } from 'konva/lib/Node';
 import { useEffect, useState } from 'react';
 import { Ellipse, FastLayer, Line, Rect, Stage, Text } from 'react-konva';
 import { GomokuBoardCell } from 'shared/game/gomoku/enums/gomokuBoardCell';
@@ -12,17 +13,18 @@ import { Level } from '../shared/game/gomoku/level';
 import { Player } from '../shared/game/gomoku/player';
 
 const Gomoku = ({ width, height }: { width: number; height: number }): JSX.Element => {
+    const small = 480 > width;
     const widthSize = 14;
     const heightSize = 14;
     const widthSizeHalf = Math.floor(widthSize / 2);
     const heightSizeHalf = Math.floor(heightSize / 2);
-    const strokeWidth = 4;
+    const strokeWidth = small ? 2 : 4;
     const strokeWidthHalf = strokeWidth / 2;
     const cellWidth = (width - strokeWidth * 2 - strokeWidth * widthSize) / widthSize;
     const cellHeight = (height - strokeWidth * 2 - strokeWidth * heightSize) / heightSize;
     const cellWidthHalf = cellWidth / 2;
     const cellHeightHalf = cellHeight / 2;
-    const textAreaHeight = 80;
+    const textAreaHeight = small ? 44 : 80;
 
     const [gomokuManager] = useState<GomokuManager>(new GomokuManager(widthSize, heightSize));
     const [coordinates, setCoordinates] = useState<Coordinate[]>(convertCellsToCoordinates(gomokuManager.board.cells));
@@ -44,6 +46,43 @@ const Gomoku = ({ width, height }: { width: number; height: number }): JSX.Eleme
         setCoordinates((_) => convertCellsToCoordinates(gomokuManager.board.cells));
     };
 
+    const select = async (e: KonvaEventObject<Event>): Promise<void> => {
+        if (!canClick) {
+            return;
+        }
+
+        const [x, y] = convert(e);
+        if (gomokuManager.next(x, y)) {
+            setCanClick((_) => false);
+
+            setCoordinates((_) => convertCellsToCoordinates(gomokuManager.board.cells));
+            sound();
+
+            if (!gomokuManager.isFinished) {
+                await CommonUtility.delay(500);
+                await gomokuManager.nextByAI();
+
+                setCoordinates((_) => convertCellsToCoordinates(gomokuManager.board.cells));
+                sound();
+            }
+
+            setCanClick((_) => true);
+        }
+
+        function convert(e: KonvaEventObject<Event>): number[] {
+            const stage = e.target.getStage();
+            const position = stage?.getPointerPosition();
+            if (!position) {
+                return [-1, -1];
+            }
+
+            const x = Math.floor((position.x - strokeWidth) / (cellWidth + strokeWidth));
+            const y = Math.floor((position.y - strokeWidth) / (cellHeight + strokeWidth));
+
+            return [x, y];
+        }
+    };
+
     useEffect(() => {
         initialize();
     }, [player, level]);
@@ -51,35 +90,7 @@ const Gomoku = ({ width, height }: { width: number; height: number }): JSX.Eleme
     return (
         <>
             <div className="flex flex-col justify-center gap-4">
-                <Stage
-                    width={width}
-                    height={height + textAreaHeight}
-                    onClick={async (e) => {
-                        if (!canClick) {
-                            return;
-                        }
-
-                        const x = Math.floor((e.evt.offsetX - strokeWidth) / (cellWidth + strokeWidth));
-                        const y = Math.floor((e.evt.offsetY - strokeWidth) / (cellHeight + strokeWidth));
-
-                        if (gomokuManager.next(x, y)) {
-                            setCanClick((_) => false);
-
-                            setCoordinates((_) => convertCellsToCoordinates(gomokuManager.board.cells));
-                            sound();
-
-                            if (!gomokuManager.isFinished) {
-                                await CommonUtility.delay(500);
-                                await gomokuManager.nextByAI();
-
-                                setCoordinates((_) => convertCellsToCoordinates(gomokuManager.board.cells));
-                                sound();
-                            }
-
-                            setCanClick((_) => true);
-                        }
-                    }}
-                >
+                <Stage width={width} height={height + textAreaHeight} onClick={select} onTap={select}>
                     <FastLayer key="gomoku-board-layer">
                         <Rect fill="#f5deb3" width={width} height={height} />
                         <Rect
@@ -154,19 +165,20 @@ const Gomoku = ({ width, height }: { width: number; height: number }): JSX.Eleme
                             width={width}
                             height={textAreaHeight}
                             fill="white"
-                            fontSize={32}
+                            fontSize={small ? 18 : 32}
+                            fontStyle={small ? 'bold' : 'normal'}
                             align="center"
                             verticalAlign="middle"
                         />
                     </FastLayer>
                 </Stage>
-                <div className="flex justify-center gap-12 border-4 border-gray-600 bg-gray-300 py-4">
-                    <FormControl sx={{ flexDirection: 'row', alignItems: 'center', gap: '1rem' }}>
-                        <FormLabel id="radio-buttons-group-label" sx={{ fontWeight: 'bold', fontSize: 20 }}>
+                <div className="flex justify-center border-2 border-gray-600 bg-gray-300 py-2 sm:gap-12 sm:border-4 sm:py-4">
+                    <FormControl sx={{ flexDirection: small ? 'column' : 'row', alignItems: 'center', gap: small ? '0.25rem' : '1rem' }}>
+                        <FormLabel id="radio-buttons-group-label" sx={{ fontWeight: 'bold', fontSize: small ? 16 : 20 }}>
                             順番
                         </FormLabel>
                         <RadioGroup
-                            row
+                            row={!small}
                             aria-labelledby="radio-buttons-group-label"
                             value={player}
                             onChange={async (e) => {
@@ -177,18 +189,27 @@ const Gomoku = ({ width, height }: { width: number; height: number }): JSX.Eleme
                                 setPlayer(e.target.value as Player);
                             }}
                         >
-                            <FormControlLabel value="black" control={<Radio />} label="先手 (黒)" />
-                            <FormControlLabel value="white" control={<Radio />} label="後手 (白)" />
+                            <FormControlLabel
+                                value="black"
+                                control={<Radio size={small ? 'small' : 'medium'} />}
+                                label={<Typography sx={{ fontSize: small ? 14 : 16 }}>先手 (黒)</Typography>}
+                            />
+                            <FormControlLabel
+                                value="white"
+                                control={<Radio size={small ? 'small' : 'medium'} />}
+                                label={<Typography sx={{ fontSize: small ? 14 : 16 }}>後手 (白)</Typography>}
+                            />
                         </RadioGroup>
                     </FormControl>
-                    <FormControl sx={{ flexDirection: 'row', alignItems: 'center', gap: '1rem', m: 1, minWidth: 100 }}>
-                        <FormLabel id="radio-buttons-group-label" sx={{ fontWeight: 'bold', fontSize: 20 }}>
+                    <FormControl sx={{ flexDirection: small ? 'column' : 'row', alignItems: 'center', gap: '1rem', minWidth: 100 }}>
+                        <FormLabel id="radio-buttons-group-label" sx={{ fontWeight: 'bold', fontSize: small ? 16 : 20 }}>
                             難易度
                         </FormLabel>
                         <Select
                             labelId="simple-select-label"
                             id="simple-select"
                             value={level}
+                            sx={{ fontSize: small ? 14 : 16 }}
                             onChange={async (e) => {
                                 if (!canClick) {
                                     return;
@@ -201,13 +222,13 @@ const Gomoku = ({ width, height }: { width: number; height: number }): JSX.Eleme
                         </Select>
                     </FormControl>
                 </div>
-                <div className="flex justify-end">
+                <div className="flex justify-center sm:justify-end">
                     <Button
-                        className="h-12 w-48"
+                        className="h-10 w-32 sm:h-12 sm:w-48"
                         fullWidth={false}
                         variant="contained"
                         color="warning"
-                        style={{ fontSize: 24 }}
+                        style={{ fontSize: small ? 18 : 24, fontWeight: small ? 'bold' : 'normal' }}
                         onClick={async () => {
                             if (!canClick) {
                                 return;
