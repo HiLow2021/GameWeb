ca033a70924e96011610f6718072bae400148060
HiLow <newbie_wisdom@hotmail.com>
2022-09-20 20:01:57 +0900

    clean

--- front/components/gameProvider.tsx
+++ front/components/gameProvider.tsx
@@ -1,14 +1,14 @@
 import dynamic from 'next/dynamic';
 import { detectWindowSize } from '../shared/windowUtility';
 
-const GameProvider = ({ gameComponentName }: { gameComponentName: string }): JSX.Element => {
+const GameProvider = ({ componentName }: { componentName: string }): JSX.Element => {
     const { width } = detectWindowSize();
     const componentWidth = width == undefined || width >= 640 ? 600 : 320;
     const componentHeight = width == undefined || width >= 640 ? 600 : 320;
 
     // konva が ES Module に対応していないことによるエラーの対応措置。
     // https://qiita.com/koji-koji/items/6b71c1579e1a77d44b4b
-    const GameComponent = dynamic<{ width: number; height: number }>(() => import(`./${gameComponentName}`), {
+    const GameComponent = dynamic<{ width: number; height: number }>(() => import(`./${componentName}`), {
         ssr: false
     });
 
