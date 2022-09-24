03c9495d5c1ea85f362e702a261236c3048d9216
HiLow <newbie_wisdom@hotmail.com>
2022-09-24 19:42:18 +0900

    add sound-toggle-button

--- front/components/layout.tsx
+++ front/components/layout.tsx
@@ -1,10 +1,14 @@
 import { Environment } from '../shared/environment';
+import SoundToggleButton from './soundToggleButton';
 
 export const Layout = ({ children }: { children: React.ReactNode }): JSX.Element => {
     return (
         <>
-            <header className="h-18 top-0 bg-black p-3 text-gray-300">
+            <header className="h-18 top-0 flex items-center justify-center bg-black p-3 text-gray-300">
                 <p className="text-center text-3xl sm:text-4xl">{Environment.siteTitle}</p>
+                <div className="absolute left-4 sm:left-8">
+                    <SoundToggleButton />
+                </div>
             </header>
             <main className="mx-auto flex min-h-screen w-full max-w-screen-xl flex-grow p-4">
                 <div className="w-full py-4 px-8">{children}</div>
--- front/shared/utility/componentUtility.ts
+++ front/shared/utility/componentUtility.ts
@@ -2,13 +2,25 @@ import { detectWindowSizeSync } from './windowUtility';
 
 type ComponentSize = { width: number; height: number; small: boolean };
 
-export const getComponentSize = (): ComponentSize => {
+export const IsSmallSize = (): boolean => {
     const windowSize = detectWindowSizeSync();
-
     const conditionWidth = 640;
-    const small = windowSize.width == undefined || windowSize.width < conditionWidth;
+
+    return windowSize.width == undefined || windowSize.width < conditionWidth;
+};
+
+export const getGameComponentSize = (): ComponentSize => {
+    const small = IsSmallSize()
     const width = small ? 320 : 600;
     const height = small ? 320 : 600;
+
+    return { width, height, small };
+};
+
+export const getSvgSize = (): ComponentSize => {
+    const small = IsSmallSize()
+    const width = small ? 36 : 48;
+    const height = small ? 36 : 48;
 
     return { width, height, small };
 };
