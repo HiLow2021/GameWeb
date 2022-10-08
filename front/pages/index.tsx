8be30f09d23d384d24f99fdd605188348ccd5120
HiLow <newbie_wisdom@hotmail.com>
2022-10-08 20:55:31 +0900

    update

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -6,17 +6,19 @@ import { GameList } from '../shared/game/gameList';
 const Index: NextPage = (): JSX.Element => {
     return (
         <Layout>
-            <div className="flex flex-col justify-center gap-4 sm:flex-row sm:gap-8">
-                {GameList.map((game) => (
-                    <Link href={game.path}>
-                        <a>
-                            <div className="flex flex-col items-center">
-                                <img className="h-36 w-36 object-scale-down sm:h-48 sm:w-48" src={game.thumbnail} alt={game.title} />
-                                <div className="p-4 text-center text-xl">{game.title}</div>
-                            </div>
-                        </a>
-                    </Link>
-                ))}
+            <div className="flex justify-center">
+                <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-16 lg:grid-cols-3">
+                    {GameList.map((game) => (
+                        <Link href={game.path}>
+                            <a>
+                                <div className="flex flex-col items-center">
+                                    <img className="h-36 w-36 object-scale-down sm:h-48 sm:w-48" src={game.thumbnail} alt={game.title} />
+                                    <div className="p-4 text-center text-xl">{game.title}</div>
+                                </div>
+                            </a>
+                        </Link>
+                    ))}
+                </div>
             </div>
         </Layout>
     );
