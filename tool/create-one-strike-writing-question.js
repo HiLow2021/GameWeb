c7552d72297299c449da57249719394b1c230c38
HiLow <newbie_wisdom@hotmail.com>
2022-10-20 10:28:01 +0900

    update

--- tool/create-one-strike-writing-question.js
+++ tool/create-one-strike-writing-question.js
@@ -1,21 +1,27 @@
 import { OneStrokeWritingGenerator } from 'shared/game/oneStrokeWriting/oneStrokeWritingGenerator';
 import { CommonUtility } from 'shared/utility/commonUtility';
-import { getFileNameWithoutExtension, writeFile } from './shared.js';
+import { getFileNameWithoutExtension, readFile, appendFile, writeFile } from './shared.js';
+
+const baseFileName = getFileNameWithoutExtension(import.meta.url);
+let total = 0;
 
 const width = { min: 4, max: 4 };
 const height = { min: 4, max: 4 };
-const block = { min: 1, max: 1 };
 const straight = [true];
-const count = 1;
+const count = 20;
 const tryCount = 100;
-const questions = [];
+
+saveQuestionSql('insert into table_name(width,height,block,straight,cells) values \n');
 
 for (const s of straight) {
     for (let x = width.min; x <= width.max; x++) {
         for (let y = height.min; y <= height.max; y++) {
             const generator = new OneStrokeWritingGenerator(x, y, s);
 
-            for (let b = block.min; b <= block.max; b++) {
+            const mean = Math.floor((x + y) / 2);
+            for (let b = Math.floor(mean / 2); b <= mean; b++) {
+                const questions = [];
+
                 for (let i = 0; i < count; i++) {
                     const result = generator.generate(b, tryCount);
                     if (result) {
@@ -23,22 +29,68 @@ for (const s of straight) {
                         CommonUtility.copy2Array(generator.board.cells, cells);
 
                         if (!checkDuplication(questions, cells)) {
-                            questions.push({ width: x, height: y, block: b, straight: s, cells })
+                            const question = { width: x, height: y, block: b, straight: s, cells };
+                            questions.push(question);
+
+                            saveQuestionJson(question);
+                            saveQuestionSql(`(${x},${y},${b},${Number(s)},'${JSON.stringify(cells)}'),\n`);
                         }
                     }
                 }
+
+                total += questions.length;
+                console.log(`generated: ${questions.length}, width: ${x}, height: ${y}, block: ${b}, straight: ${s}`);
             }
         }
     }
 }
 
-writeFile(getFileNameWithoutExtension(import.meta.url) + '.json', JSON.stringify(questions, null, 4));
+fixSqlSuffix();
+console.log(`total generated: ${total}`);
 
 function checkDuplication(questions, target) {
+    let result = false;
     const questionCells = questions.map((x) => JSON.stringify(x.cells.flat()));
-    const cells = JSON.stringify(target.flat());
 
-    questionCells.push(cells);
+    result ||= check(questionCells, target);
 
-    return new Set(questionCells).size !== questionCells.length;
+    if (target.length === target[0].length) {
+        result ||= check(questionCells, CommonUtility.rotate90(target));
+        result ||= check(questionCells, CommonUtility.rotate270(target));
+        result ||= check(questionCells, CommonUtility.transpose(target));
+        result ||= check(questionCells, CommonUtility.rotate90(CommonUtility.transpose(target)));
+        result ||= check(questionCells, CommonUtility.rotate180(CommonUtility.transpose(target)));
+        result ||= check(questionCells, CommonUtility.rotate270(CommonUtility.transpose(target)));
+    }
+
+    result ||= check(questionCells, CommonUtility.rotate180(target));
+
+    return result;
+
+    function check(questions, target) {
+        questions.push(JSON.stringify(target.flat()));
+        const result = new Set(questionCells).size !== questionCells.length;
+        questions.pop();
+
+        return result;
+    }
+}
+
+function saveQuestionJson(question) {
+    const jsonString = readFile(baseFileName + '.json');
+    const json = jsonString ? JSON.parse(jsonString) : [];
+    json.push(question);
+
+    writeFile(baseFileName + '.json', JSON.stringify(json, null, 4));
+}
+
+function saveQuestionSql(text) {
+    appendFile(baseFileName + '.sql', text);
+}
+
+function fixSqlSuffix() {
+    const sql = readFile(baseFileName + '.sql');
+    const sqlString = sql ? sql.toString('utf8', 0, sql.length) : '';
+
+    writeFile(baseFileName + '.sql', sqlString.replace(/,\s*?$/, ';'));
 }
--- tool/shared.js
+++ tool/shared.js
@@ -2,6 +2,8 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+const outDirectory = './out/';
+
 export function getFullFileName(url) {
     return fileURLToPath(url);
 }
@@ -18,8 +20,23 @@ export function getDirectoryName(url) {
     return path.dirname(getFullFileName(url));
 }
 
+export function appendFile(fileName, data) {
+    if (!fs.existsSync(outDirectory)) {
+        fs.mkdirSync(outDirectory);
+    }
+
+    fs.appendFileSync(outDirectory + fileName, data);
+}
+
+export function readFile(fileName) {
+    if (!fs.existsSync(outDirectory + fileName)) {
+        return undefined;
+    }
+
+    return fs.readFileSync(outDirectory + fileName);
+}
+
 export function writeFile(fileName, data) {
-    const outDirectory = './out/';
     if (!fs.existsSync(outDirectory)) {
         fs.mkdirSync(outDirectory);
     }
