5b1b4eb4d21aa128a4ce29afbd6371955f9203d5
HiLow <newbie_wisdom@hotmail.com>
2023-06-29 14:59:32 +0900

    update

--- front/pages/connectFour/index.tsx
+++ front/pages/connectFour/index.tsx
@@ -1,10 +1,20 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 import GameProvider from '../../components/gameProvider';
 import { Layout } from '../../components/layout';
+import { SitePage } from '../../shared/sitePage';
 
 const Index: NextPage = (): JSX.Element => {
     return (
         <Layout>
+            <Head>
+                <title>{SitePage.connectFour.title}</title>
+                <meta name="description" content={SitePage.connectFour.description} />
+                <meta property="og:title" content={SitePage.connectFour.title} />
+                <meta property="og:url" content={SitePage.connectFour.url} />
+                <meta property="og:description" content={SitePage.connectFour.description} />
+                <meta property="og:image" content={SitePage.connectFour.image} />
+            </Head>
             <GameProvider componentName="connectFour" />
         </Layout>
     );
--- front/pages/lightsOut/index.tsx
+++ front/pages/lightsOut/index.tsx
@@ -1,10 +1,20 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 import GameProvider from '../../components/gameProvider';
 import { Layout } from '../../components/layout';
+import { SitePage } from '../../shared/sitePage';
 
 const Index: NextPage = (): JSX.Element => {
     return (
         <Layout>
+            <Head>
+                <title>{SitePage.lightsOut.title}</title>
+                <meta name="description" content={SitePage.lightsOut.description} />
+                <meta property="og:title" content={SitePage.lightsOut.title} />
+                <meta property="og:url" content={SitePage.lightsOut.url} />
+                <meta property="og:description" content={SitePage.lightsOut.description} />
+                <meta property="og:image" content={SitePage.lightsOut.image} />
+            </Head>
             <GameProvider componentName="lightsOut" />
         </Layout>
     );
