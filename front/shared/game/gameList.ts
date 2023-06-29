d60f72797efb04becf6cd3e9e07e05476ad282ac
HiLow <newbie_wisdom@hotmail.com>
2023-06-29 12:43:54 +0900

    update

--- front/shared/game/gameList.ts
+++ front/shared/game/gameList.ts
@@ -1,10 +1,10 @@
 import { Game } from './game';
 
 export const GameList: Readonly<Game[]> = [
-    { title: 'オセロ', path: '/othello', thumbnail: 'game/othello/thumbnail.png' },
-    { title: '五目並べ', path: '/gomoku', thumbnail: 'game/gomoku/thumbnail.png' },
-    { title: 'コネクトフォー', path: '/connectFour', thumbnail: 'game/connectFour/thumbnail.png' },
-    { title: 'スライドパズル', path: '/slidingPuzzle', thumbnail: 'game/slidingPuzzle/thumbnail.png' },
-    { title: '一筆書きパズル', path: '/oneStrokeWriting', thumbnail: 'game/oneStrokeWriting/thumbnail.png' },
-    { title: 'ライツアウト', path: '/lightsOut', thumbnail: 'game/lightsOut/thumbnail.png' }
+    { title: 'オセロ', path: '/othello', thumbnail: 'game/othello/thumbnail.webp' },
+    { title: '五目並べ', path: '/gomoku', thumbnail: 'game/gomoku/thumbnail.webp' },
+    { title: 'コネクトフォー', path: '/connectFour', thumbnail: 'game/connectFour/thumbnail.webp' },
+    { title: 'スライドパズル', path: '/slidingPuzzle', thumbnail: 'game/slidingPuzzle/thumbnail.webp' },
+    { title: '一筆書きパズル', path: '/oneStrokeWriting', thumbnail: 'game/oneStrokeWriting/thumbnail.webp' },
+    { title: 'ライツアウト', path: '/lightsOut', thumbnail: 'game/lightsOut/thumbnail.webp' }
 ] as const;
