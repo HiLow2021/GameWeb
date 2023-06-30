968a9befd00158debae6eaff9c327ad3f1c03e6a
HiLow <newbie_wisdom@hotmail.com>
2023-07-01 01:29:14 +0900

    clean

--- front/components/soundToggleButton.tsx
+++ front/components/soundToggleButton.tsx
@@ -2,13 +2,14 @@ import { IconButton } from '@mui/material';
 import Image from 'next/image';
 import { useContext, useEffect, useState } from 'react';
 import SoundStateContext from '../contexts/soundStateContext';
+import { SitePath } from '../shared/sitePath';
 import { getIconSize } from '../shared/utility/componentUtility';
 
 const SoundToggleButton = (): JSX.Element => {
     const { width, height } = getIconSize();
 
     const { currentSoundState, changeSoundState } = useContext(SoundStateContext);
-    const imageSource = currentSoundState ? '/volume-on.svg' : '/volume-off.svg';
+    const imageSource = currentSoundState ? SitePath.public.file.volume.on : SitePath.public.file.volume.off;
 
     const [mounted, setMounted] = useState(false);
     useEffect(() => setMounted(true), []);
