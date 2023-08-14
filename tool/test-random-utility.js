fbf0be5b07e40f4c7ce4c5974c244625ec5a5aee
HiLow <newbie_wisdom@hotmail.com>
2023-08-14 16:53:47 +0900

    delete

--- tool/test-random-utility.js
+++
@@ -1,9 +0,0 @@
-import { RandomUtility } from 'shared/utility/randomUtility';
-import { getFileNameWithoutExtension, writeFile } from './shared.js';
-
-const min = 0;
-const max = 2
-const count = 100;
-const result = [...Array(count)].map(() => RandomUtility.random(min, max))
-
-writeFile(getFileNameWithoutExtension(import.meta.url) + '.json', JSON.stringify(result, null, 4));
